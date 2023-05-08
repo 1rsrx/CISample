326f6b4cdee836ef87a0cda9fec5d7cc224be8e6
Hikaru Kurodq <h1kqaa@gmail.com>
2023-05-09 08:32:47 +0900

    test

---
+++ CISample/CISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CISampleApp.swift
+//  CISample
+//
+//  Created by Hikaru Kuroda on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct CISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
